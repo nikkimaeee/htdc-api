a473eb35cd66c9fe9f4779773311c9148987cdd2
nikkimaeee <nikkimaeee46@gmail.com>
2024-11-13 00:55:00 +0800

    update email sender name

--- htdc-api/Services/EmailSender.cs
+++ htdc-api/Services/EmailSender.cs
@@ -21,7 +21,7 @@ public class EmailSender : IEmailSender
     private MimeMessage CreateEmailMessage(Message message)
     {
         var emailMessage = new MimeMessage();
-        emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+        emailMessage.From.Add(new MailboxAddress("Ocampo Dentail Clinic", _emailConfig.From));
         emailMessage.To.AddRange(message.To);
         emailMessage.Subject = message.Subject;
         emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message.Content };
